14fc98bb6f56035324512c8c66bf26f5de863f73
devlets <leandrovictor2011@gmail.com>
2026-01-15 22:11:47 -0300

    implementação do Zod para validação

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,63 @@
+import { Link } from "react-router-dom";
+import { Container } from "../../components/container";
+import logoImg from "../../assets/logo.png";
+
+import { Input } from "../../components/input/index";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .email("Insira um email válido")
+    .min(1, "O campo email é obrigatório"),
+  password: z.string().min(1, "O campo senha é obrigatório"),
+});
+type FormData = z.infer<typeof schema>;
+
 export function Login() {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logoImg} className="w-full" alt="" />
+        </Link>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="bg-white max-w-xl w-full rounded-lg"
+        >
+          <div className="mb-3">
+            <Input
+              type="text"
+              placeholder="Digite seu email"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            ></Input>
+
+            <Input
+              type="password"
+              placeholder="Digite sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            ></Input>
+          </div>
+          <button className="cursor-pointer">Acessar</button>
+        </form>
+      </div>
+    </Container>
+  );
 }
